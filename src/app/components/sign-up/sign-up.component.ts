ac1e940c31f6f6e728d99c62c4f29081634adfa9
Vitalii <vitalikdzyuba0106@gmail.com>
2024-03-22 01:00:44 +0200

    Add Sign Up & Login

--- src/app/components/sign-up/sign-up.component.ts
+++ src/app/components/sign-up/sign-up.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-sign-up',
@@ -9,4 +10,9 @@ import { Component } from '@angular/core';
 })
 export class SignUpComponent {
 
+  constructor(private router : Router) {
+  }
+  navigateTo(route: string) {
+    this.router.navigate([`/${route}`]);
+  }
 }
